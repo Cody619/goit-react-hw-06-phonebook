07b34a7c593daea241ec31ce4447d6480121f4f2
Codyfs <codyfs619@gmail.com>
2022-02-22 13:55:46 +0200

    homework

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { v4 as uuidv4 } from 'uuid'
 import { PhoneBook } from './componets/PhoneBook.js'
